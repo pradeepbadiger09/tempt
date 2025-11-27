a3650e6dc1ed64db72b668bf520510738018ea9b
pradeepbadiger09 <pradeepbadiger618@gmail.com>
2025-11-27 14:49:10 +0530

    Update tempt.py

--- tempt.py
+++ tempt.py
@@ -1,11 +1,17 @@
 import sys
-if len (sys.argv) != 2:
-print("Usage: python temp_alert.py ‹temperi
-sys.exit()
+
+# Check if argument count is correct
+if len(sys.argv) != 2:
+    print("Usage: python temp_alert.py <temperature>")
+    sys.exit()
+
+# Convert input to float
 temp = float(sys.argv[1])
+
+# Temperature conditions
 if temp < 15:
-print("Cold")
+    print("Cold")
 elif 15 <= temp <= 30:
-print("Normal")
+    print("Normal")
 else:
-print("Hot")
+    print("Hot")
