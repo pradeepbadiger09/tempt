be9e3726588e434c7fcec9ce159a2cffc87dd53b
pradeepbadiger09 <pradeepbadiger618@gmail.com>
2025-11-27 14:42:57 +0530

    Create tempt.py

---
+++ tempt.py
@@ -0,0 +1,11 @@
+import sys
+if len (sys.argv) != 2:
+print("Usage: python temp_alert.py ‹temperi
+sys.exit()
+temp = float(sys.argv[1])
+if temp < 15:
+print("Cold")
+elif 15 <= temp <= 30:
+print("Normal")
+else:
+print("Hot")
